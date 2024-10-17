14bc2a14a15ff73680d8dec1041c1926b06cbd70
ecarnovsky <ecarnovsky@gmail.com>
2024-10-17 14:19:40 -0400

    first commit

--- app/src/main/java/com/csi2818/statecapitals/Capital.kt
+++ app/src/main/java/com/csi2818/statecapitals/Capital.kt
@@ -1,4 +1,18 @@
 package com.csi2818.statecapitals
+class Capital(state : String, capitalCity : String) {
 
-class Capital {
+    var state : String = ""
+        get() = field
+        set(value) {
+            field = value
+        }
+    var capitalCity : String = ""
+        get() = field
+        set(value) {
+            field = value
+        }
+    init {
+        this.state = state
+        this.capitalCity = capitalCity
+    }
 }
